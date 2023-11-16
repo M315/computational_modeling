04037bfeaee797cf98f6d4c3cc7427592d20892f
marcjovani <marc.jovanibertran@gmail.com>
2023-11-17 00:08:34 +0100

    Stats

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use polars_core::export::chrono::format::Numeric;
 use polars_core::prelude::StaticArray;
 use rand::*;
 use rand::distributions::{Bernoulli, Distribution};
-use rand::rngs::ThreadRng;
+use rand::rngs::*;
 
 pub mod data_utils;
 pub mod models;
 
-fn bernoulli_experiment(rng: &mut ThreadRng, p: f64, n: usize) -> u64 {
+fn bernoulli_experiment(rng: &mut StdRng, p: f64, n: usize) -> u64 {
     let bern: Bernoulli = Bernoulli::new(p).unwrap();
     bern.sample_iter(rng)
         .take(n)
@@ -15,7 +15,7 @@ fn bernoulli_experiment(rng: &mut ThreadRng, p: f64, n: usize) -> u64 {
         .sum()
 }
 
-fn random_walk(rng: &mut ThreadRng, p: f64, n: usize) -> Vec<i64> {
+fn random_walk(rng: &mut SmallRng, p: f64, n: usize) -> Vec<i64> {
     let bern: Bernoulli = Bernoulli::new(p).unwrap();
     bern.sample_iter(rng)
         .take(n)
@@ -26,11 +26,14 @@ fn random_walk(rng: &mut ThreadRng, p: f64, n: usize) -> Vec<i64> {
         .collect()
 }
 
-fn random_walk_end(rng: &mut ThreadRng, p: f64, n: usize) -> i64 {
+fn random_walk_end(rng: &mut SmallRng, p: f64, n: usize, m: usize) -> Vec<i64> {
     let bern: Bernoulli = Bernoulli::new(p).unwrap();
-    bern.sample_iter(rng)
-        .take(n)
-        .fold(0, |acc, x| acc + (2 * (x as i64) - 1))
+    let r: Vec<bool> = bern.sample_iter(rng)
+        .take(n * m)
+        .collect();
+    r.chunks(n)
+        .map(|v| v.into_iter().fold(0, |acc, &x| acc + (2 * (x as i64) - 1)))
+        .collect()
 }
 
 fn main() {
@@ -38,7 +41,7 @@ fn main() {
     let walkers: usize = 10;
 
     // init rng
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rngs::SmallRng::from_entropy();
 
     // collect the results into a vector:
     let rw: Vec<Vec<i64>> = (0..walkers).map(|_| random_walk(&mut rng, 0.5, steps)).collect();
@@ -52,22 +55,22 @@ fn main() {
     plot.set_layout(layout);
     plot.write_html("random_walks.html");
 
-    let steps: usize = 1000;
-    let walkers: usize = 100000;
+    let steps: usize = 1_000;
+    let walkers: usize = 100_000;
 
     // collect the results into a vector:
-    let rw_end: Vec<i64> = (0..walkers).map(|_| random_walk_end(&mut rng, 0.5, steps)).collect();
+    let rw_end: Vec<i64> = random_walk_end(&mut rng, 0.5, steps, walkers);
 
-    let mut plot = plotly::Plot::new();
-    plot.add_trace(plotly::Scatter::new((0..walkers).collect(), rw_end.to_vec()).mode(plotly::common::Mode::Markers));
-    let layout = plotly::Layout::new().x_axis(plotly::layout::Axis::new().show_grid(false))
-                                              .y_axis(plotly::layout::Axis::new().show_grid(false));
-    plot.set_layout(layout);
-    plot.write_html("random_walks_end.html");
+    // let mut plot = plotly::Plot::new();
+    // plot.add_trace(plotly::Scatter::new((0..walkers).collect(), rw_end.to_vec()).mode(plotly::common::Mode::Markers));
+    // let layout = plotly::Layout::new().x_axis(plotly::layout::Axis::new().show_grid(false))
+    //                                           .y_axis(plotly::layout::Axis::new().show_grid(false));
+    // plot.set_layout(layout);
+    // plot.write_html("random_walks_end.html");
 
     let min: i64 = *rw_end.iter().min().unwrap();
     let max: i64 = *rw_end.iter().max().unwrap();
-    let rw_end_dist: Vec<i64> = rw_end.into_iter().fold(vec![0; (max - min + 1) as usize], |mut state, x| 
+    let rw_end_dist: Vec<i64> = rw_end.iter().fold(vec![0; (max - min + 1) as usize], |mut state, x| 
         {
             state[(x - min) as usize] += 1;
             state
@@ -80,5 +83,12 @@ fn main() {
     plot.set_layout(layout);
     plot.write_html("random_walks_end_dist.html");
 
+    let mut plot = plotly::Plot::new();
+    plot.add_trace(plotly::Histogram::new(rw_end.to_vec()).name("h"));
+    let layout = plotly::Layout::new().x_axis(plotly::layout::Axis::new().show_grid(false))
+                                              .y_axis(plotly::layout::Axis::new().show_grid(false));
+    plot.set_layout(layout);
+    plot.write_html("random_walks_end_hist.html");
 
+    println!("{:?}", rw_end.iter().sum::<i64>() as f64 / rw_end.len() as f64);
 }
